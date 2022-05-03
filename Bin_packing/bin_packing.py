386d09b1dfefa73cb8fdcaace87e946853238874
Arthur <55024064+zlayapechenushka@users.noreply.github.com>
2022-05-04 02:17:26 +0300

    Second task solved

---
+++ Bin_packing/bin_packing.py
@@ -0,0 +1,19 @@
+if __name__ == "__main__":
+    number_of_items = int(input())
+    bin_volume = int(input())
+    items = [[int(input()), i] for i in range(number_of_items)]
+    items = sorted(items, key=lambda x: x[0], reverse=True)
+    bins = [0] * number_of_items
+    answer = [0] * number_of_items
+    print(items)
+
+    for i in range(number_of_items):
+        volume = items[i][0]
+        for k in range(len(bins)):
+            if bins[k] + volume <= bin_volume:
+                bins[k] += volume
+                answer[items[i][1]] = k + 1
+                print(f"Item {i} added to bin {k}")
+                break
+
+    print(*answer)
